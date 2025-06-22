f89a7584a70592f59e6579e765d1cf5d3e90c9f6
den.ysp <33653989+mettugi@users.noreply.github.com>
2025-06-22 10:21:13 +0700

    Create add-remove-elements-page.ts

---
+++ TheInternetHerokuApp/Page-Object/Disappearing-Elements/add-remove-elements-page.ts
@@ -0,0 +1,21 @@
+import { Locator, Page } from '@playwright/test';
+
+export class AddRemoveElementsPage {
+  readonly page: Page;
+  readonly addElementButton: Locator;
+  readonly deleteElementButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.addElementButton = page.locator("text='Add Element'");
+    this.deleteElementButton = page.locator("text='Delete'");
+  }
+
+  async addElement() {
+    await this.addElementButton.first().click();
+  }
+
+  async deleteElement() {
+    await this.deleteElementButton.first().click();
+  }
+}

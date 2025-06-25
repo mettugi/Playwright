34b3877791fa095d66459479ddbbfb2dcfe30e36
den.ysp <33653989+mettugi@users.noreply.github.com>
2025-06-25 15:19:10 +0700

    Create formData.ts

---
+++ PracticeForm/Data/formData.ts
@@ -0,0 +1,14 @@
+export const formData = {
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'john.doe@example.com',
+  gender: '1', // 1 = Male, 2 = Female, 3 = Other
+  mobile: '0812345678',
+  dob: '01 Jan 2000',
+  subjects: ['Maths', 'Physics'],
+  hobbies: ['1', '2'], // 1 = Sports, 2 = Reading, 3 = Music
+  picturePath: 'tests/data/sample.jpg', // Ensure the file exists
+  address: 'Jl. Mawar No. 123',
+  state: 'NCR',
+  city: 'Delhi'
+};

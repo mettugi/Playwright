c61d9d146a82abda4181a5981f9dd5eaa6182151
den.ysp <33653989+mettugi@users.noreply.github.com>
2025-07-08 10:20:29 +0700

    Create constants.ts

---
+++ PracticeForm/src/utils/constants.ts
@@ -0,0 +1,27 @@
+export const URLS = {
+  BASE_URL: 'https://demoqa.com',
+  PRACTICE_FORM: '/automation-practice-form'
+};
+
+export const TIMEOUTS = {
+  SHORT: 5000,
+  MEDIUM: 10000,
+  LONG: 30000
+};
+
+export const GENDER_MAP = {
+  Male: '1',
+  Female: '2',
+  Other: '3'
+};
+
+export const HOBBY_MAP = {
+  Sports: '1',
+  Reading: '2',
+  Music: '3'
+};
+
+export const VALIDATION_COLORS = {
+  ERROR: 'rgb(220, 53, 69)',
+  SUCCESS: 'rgb(40, 167, 69)'
+};

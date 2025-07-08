de2b6766dcb262a6dae5e8f5c4173f5ee68f8e49
den.ysp <33653989+mettugi@users.noreply.github.com>
2025-07-08 10:09:45 +0700

    Update playwright.config.ts

--- PracticeForm/playwright.config.ts
+++ PracticeForm/playwright.config.ts
@@ -1,12 +1,1 @@
-import { defineConfig } from '@playwright/test';
 
-export default defineConfig({
-  testDir: './tests',
-  use: {
-    headless: true,
-    viewport: { width: 1280, height: 720 },
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    baseURL: 'https://demoqa.com',
-  },
-});

75e2d7a132930ec4054aa4658cd362b1cb98a83a
den.ysp <33653989+mettugi@users.noreply.github.com>
2025-07-09 13:38:41 +0700

    Delete Restful_Booker_API directory

--- Restful_Booker_API/tests/API/helper.ts
+++
@@ -1,9 +0,0 @@
-// tests/api/helpers.ts
-import { request as playwrightRequest, APIRequestContext } from '@playwright/test';
-
-export async function createRequestContext(): Promise<APIRequestContext> {
-  return await playwrightRequest.newContext({
-    baseURL: 'https://restful-booker.herokuapp.com',
-    extraHTTPHeaders: { 'Content-Type': 'application/json' },
-  });
-}
